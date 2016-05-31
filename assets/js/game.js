f05641d99854695206bbd5c0a33824d6f82cb93f
Arnold Lei <arnold.lei88@gmail.com>
2016-05-31 18:40:08 -0400

    Still working

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
-  // name, vit, str, dex, int, ac, fury, type 
-  var a = new char('Arnold', (d(6) + 30), (d(6)+ 15), (d(6) + 15), (d(6) + 15), (d(6) + 13), (d(6) + 10), 'player') ;
-  var b = new char('Wild Troll', (d(6) + 1000), (d(6)+ 10), (d(6) + 10), (d(6) + 15), (d(6) + 13), (d(6) + 10));
+  // name, vit, str, dex, int, ac, fury, type
+  var a = new char('Arnold', (d(6) + 30), (d(6)+ 15), (d(6) + 15), (d(6) + 15), (d(6) + 13), (d(6) + 10), 'player');
+  var b = new char('Wild Troll', (d(6) + 30), (d(6)+ 10), (d(6) + 10), (d(6) + 15), (d(6) + 13), (d(6) + 10));
   printPlayerStats(a);
   printNpcStats(b);
   // a.attack(b);
